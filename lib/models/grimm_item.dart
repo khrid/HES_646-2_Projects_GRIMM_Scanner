873bfafd07bd6dcccc2c9bce1dc5b7dd9588c01a
rgallay <robin.gallay@students.hevs.ch>
2021-10-25 21:35:47 +0200

    Start of object view - bug not fix

--- lib/models/grimm_item.dart
+++ lib/models/grimm_item.dart
@@ -1,3 +1,93 @@
-class GrimmItem {
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class GrimmItem {
+  late final String id;
+
+  /// Item description
+  String description;
+
+  /// Item location
+  String location;
+
+  /// Item category
+  String idCategory;
+
+  /// Item avaibality
+  bool available;
+
+  /// Item remark
+  String remark;
+
+  /// Default constructor
+  /*GrimmItem(
+      {this.description = "",
+      this.location = "",
+      this.idCategory = "",
+      this.available = true,
+      this.remark = ""});*/
+
+  /// Default constructor
+  GrimmItem({
+    this.id = "-1",
+    required this.description,
+    required this.location,
+    required this.idCategory,
+    required this.available,
+    required this.remark,
+  });
+
+  /// Returns a readable GrimmItem object
+  String toString() {
+    return "GrimmItem{id:" +
+        id +
+        ",description:" +
+        description +
+        ",location:" +
+        location +
+        ",category:" +
+        idCategory +
+        ",remark:" +
+        remark +
+        ", available" +
+        available.toString() +
+        "}";
+  }
+
+  /// Translate a MyUser object to JSON
+  Map<String, Object?> toJson() {
+    return {
+      //'id': id,
+      'description': description,
+      'location': location,
+      'idCategory': idCategory,
+      'remark': remark,
+      'available': available
+    };
+  }
+
+  /// Retrieves the user info from Firebase document
+  Future<void> populateItemInfoFromFirestore() async {
+    DocumentSnapshot<Map<String, dynamic>> snap =
+        await FirebaseFirestore.instance.collection("items").doc(id).get();
+    if (snap.exists) {
+      description = (snap.data()!["description"] != null
+          ? snap.data()!["description"]
+          : "");
+      location =
+          (snap.data()!["location"] != null ? snap.data()!["location"] : "");
+      idCategory = (snap.data()!["idCategory"] != null
+          ? snap.data()!["idCategory"]
+          : "");
+      remark = (snap.data()!["remark"] != null ? snap.data()!["remark"] : "");
+      available = (snap.data()!["available"] != null
+          ? snap.data()!["available"]
+          : false);
+      ;
+    }
+  }
+
+  /// Set an user UID
+  void setid(String id) {
+    this.id = id;
+  }
 }
--- lib/pages/items_detail.dart
+++ lib/pages/items_detail.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:grimm_scanner/models/grimm_item.dart';
 import 'package:grimm_scanner/utils/qrutils.dart';
+import 'package:grimm_scanner/pages/home.dart';
 
 class ItemDetail extends StatefulWidget {
   static const routeName = "/items/detail";
@@ -15,6 +18,9 @@ class _ItemDetailState extends State<ItemDetail> {
   @override
   Widget build(BuildContext context) {
     final qrcode = ModalRoute.of(context)!.settings.arguments as String;
+
+    final grimmItem = ModalRoute.of(context)!.settings.arguments as GrimmItem;
+    print("Item Object " + grimmItem.toString());
 
     // TODO récupérer les infos de Firebase avec le contenu de "qrcode"
 
@@ -36,9 +42,89 @@ class _ItemDetailState extends State<ItemDetail> {
                 QRUtils.generateQrWidgetFromString(qrcode),
                 const SizedBox(height: 20.0),
                 Text(qrcode),
+                Padding(
+                  padding: EdgeInsets.only(top: 40),
+                ),
+                Expanded(
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('items')
+                        .doc(grimmItem.id)
+                        .snapshots(),
+                    builder: buildItemDetails,
+                  ),
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      textStyle: TextStyle(
+                          fontFamily: "Raleway-Regular", fontSize: 14.0),
+                      side: const BorderSide(width: 1.0, color: Colors.black),
+                      padding: EdgeInsets.all(10.0),
+                    ),
+                    onPressed: () async {
+                      // button for emprunter
+                    },
+                    child: Text("EMPRUNTER")),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      textStyle: TextStyle(
+                          fontFamily: "Raleway-Regular", fontSize: 14.0),
+                      side: const BorderSide(width: 1.0, color: Colors.black),
+                      padding: EdgeInsets.all(10.0),
+                    ),
+                    onPressed: () async {
+                      // button for retourner
+                    },
+                    child: Text("RETOURNER")),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      textStyle: TextStyle(
+                          fontFamily: "Raleway-Regular", fontSize: 14.0),
+                      side: const BorderSide(width: 1.0, color: Colors.black),
+                      padding: EdgeInsets.all(10.0),
+                    ),
+                    onPressed: () async {
+                      // button for autre scan
+                      Navigator.pushNamed(context, "/");
+                    },
+                    child: Text("MENU")),
               ],
             ),
           ],
         ))));
+  }
+}
+
+Widget buildItemDetails(
+    BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+  if (snapshot.hasData) {
+    var grimmItem = snapshot.data;
+    GrimmItem item = GrimmItem(
+      description: grimmItem!["description"],
+      location: grimmItem["location"],
+      idCategory: grimmItem["idCategory"],
+      remark: grimmItem["remark"],
+      available: grimmItem["available"],
+    );
+    return Container(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text("Emplacement: " + item.location,
+          style: TextStyle(color: Colors.black, fontSize: 14)),
+      const SizedBox(height: 20.0),
+      Text("Catégorie: ", style: TextStyle(color: Colors.black, fontSize: 14)),
+      const SizedBox(height: 20.0),
+      Text("Date d'emprunt: ",
+          style: TextStyle(color: Colors.black, fontSize: 14)),
+      const SizedBox(height: 20.0),
+      Text("Status: ", style: TextStyle(color: Colors.black, fontSize: 14)),
+      const SizedBox(height: 20.0),
+    ]));
+  } else {
+    return Text("No item details yet :(");
   }
 }
